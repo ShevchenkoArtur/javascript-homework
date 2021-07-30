9f0e2fbec6e5ccd66a7b01384e7df93c045f81c9
Artur <shevchenkoartur.745@gmail.com>
2021-07-30 14:20:57 +0300

    task3 done

--- main.js
+++ main.js
@@ -17,3 +17,53 @@ for (let i = 0; i < arrOfCity.length; i++) {
 }
 
 console.log(result)
+
+// task3
+const namesOfDays = {
+    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+}
+
+const switchDayOfWeek = (lang, day) => {
+    switch (day) {
+        case 1:
+            return namesOfDays[lang][day - 1]
+        case 2:
+            return namesOfDays[lang][day - 1]
+        case 3:
+            return namesOfDays[lang][day - 1]
+        case 4:
+            return namesOfDays[lang][day - 1]
+        case 5:
+            return namesOfDays[lang][day - 1]
+        case 6:
+            return namesOfDays[lang][day - 1]
+        case 7:
+            return namesOfDays[lang][day - 1]
+        default:
+            throw 'Введен некорректный номер дня'
+    }
+}
+
+const getNameOfDay = (lang, day) => {
+    const ruKey = Object.keys(namesOfDays)[0]
+    const enKey = Object.keys(namesOfDays)[1]
+
+    if (typeof lang !== 'string') {
+        throw 'Введены некорректные данные'
+    }
+
+    if (lang !== ruKey && lang !== enKey) {
+        throw 'Введен неверный язык'
+    }
+
+    if (lang === ruKey) {
+        return switchDayOfWeek(lang, day)
+    }
+
+    if (lang === enKey) {
+        return switchDayOfWeek(lang, day)
+    }
+}
+
+console.log(getNameOfDay('en', 7));
