63a30ec3c727a3f90a7272da0f3a000f1ef2a9f6
Artur <shevchenkoartur.745@gmail.com>
2021-08-15 19:24:31 +0300

    task4 done

--- main.js
+++ main.js
@@ -35,3 +35,27 @@ console.log(getUpdatedArr(5))
 console.log(getUpdatedArr({name: 'Vasya'}))
 console.log(getUpdatedArr())
 console.log(getUpdatedArr(4))
+
+// task4
+const addZero = num => {
+    if (num <= 9) return '0' + num
+    else return num
+}
+
+const timer = sec => {
+    let timerId = setInterval(function () {
+        if (sec === 0) {
+            clearInterval(timerId)
+            console.log('Time End')
+            return
+        }
+
+        let minutes = Math.floor(sec % 3600 / 60)
+        let seconds = Math.floor(sec % 60)
+
+        console.log(`${addZero(minutes)}:${addZero(seconds)}`)
+        sec--
+    }, 1000)
+}
+
+timer(120)
