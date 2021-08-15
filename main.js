5b4d6e2328f8a8c92a04cb36505579ac193f89d7
Artur <shevchenkoartur.745@gmail.com>
2021-08-15 19:20:38 +0300

    task2 done

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ let counter = makeCounter()
 console.log(counter(3))
 console.log(counter(5))
 console.log(counter(228))
+
+// task2
+const updateArr = () => {
+    let arr = []
+
+    return value => {
+        if (typeof value === 'undefined') {
+            arr = []
+            return arr
+        }
+
+        arr.push(value)
+        return arr
+    }
+}
+
+const getUpdatedArr = updateArr()
+
+console.log(getUpdatedArr(3))
+console.log(getUpdatedArr(5))
+console.log(getUpdatedArr({name: 'Vasya'}))
+console.log(getUpdatedArr())
+console.log(getUpdatedArr(4))
