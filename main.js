910e1dbfa122c875eb743876609c5b5a73bf7da1
Artur <shevchenkoartur.745@gmail.com>
2021-08-17 12:36:51 +0300

    task4 done

--- main.js
+++ main.js
@@ -24,10 +24,31 @@ console.log(getAllKeys(obj))
 // task3
 const getAllValues = obj => {
     const result = []
-    for(let key in obj) {
+    for (let key in obj) {
         result.push(obj[key])
     }
     return result
 }
 
 console.log(getAllValues(obj));
+
+// task4
+const newCandidate = {
+    id: 4,
+    name: 'Katya'
+}
+
+const insertIntoArr = (obj, id) => {
+    let candidateIndex = null
+
+    for (let i = 0; i < condidateArr.length; i++) {
+        if (condidateArr[i]._id === id) {
+            candidateIndex = i
+            break
+        }
+    }
+    condidateArr.splice(candidateIndex, 0, obj)
+}
+
+insertIntoArr(newCandidate, condidateArr[40]._id)
+console.log(condidateArr);
