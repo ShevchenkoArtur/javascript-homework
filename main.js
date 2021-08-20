b3b33233d06a84d506da120e1b5818ca32ee59cf
Artur <shevchenkoartur.745@gmail.com>
2021-08-20 13:18:58 +0300

    task2 done

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const searchCandidatesByPhoneNumber = value => {
 }
 
 console.log(searchCandidatesByPhoneNumber('43'));
+
+// task2
+const getCandidateById = id => {
+    const candidate = condidateArr.filter(el => el._id === id)[0]
+    const arrOfRegister = candidate.registered.split('-')
+
+    candidate.registered = `${arrOfRegister[2].substr(0, 2)}/${arrOfRegister[1]}/${arrOfRegister[0]}`
+
+    return candidate
+}
+
+console.log(getCandidateById('5e216bc997af60c69199e4ca'));
